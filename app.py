4ec26232db3cf64f6ee69ddc49ce7f3163e98237
leo-mm <leandro.merces@outlook.com>
2025-06-10 14:24:49 +0100

    Primeiro commit: projeto de análise com Streamlit

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
+import pandas as pd
+import plotly.express as px
 import streamlit as st
 
-st.header('Jogando uma moeda')
+# Cabeçalho do aplicativo
+st.header('Análise de Dados de Veículos Usados')
 
-st.write('Ainda não é um aplicativo funcional. Em construção.')
+# Carregar o conjunto de dados
+car_data = pd.read_csv("D:/Documents/vehicles.csv")
+
+# Botão para criar o histograma
+hist_button = st.button('Criar histograma')
+
+if hist_button:
+    # Mensagem ao clicar no botão
+    st.write('Criando um histograma para a quilometragem dos veículos')
+
+    # Criar o histograma
+    fig = px.histogram(car_data, x='odometer', nbins=30,
+                        title='Distribuição da Quilometragem dos Veículos')
+
+    # Exibir o histograma no aplicativo
+    st.plotly_chart(fig, use_container_width=True)
